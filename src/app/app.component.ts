9640922b0ae8a1189a54e0f5670af6832b1aeaf5
Roland Tóth <tothroland162@gmail.com>
2019-12-20 11:26:56 +0100

    Some responsivity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,114 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'js-test';
+export class AppComponent implements OnInit {
+    todos: Array<any> = [];
+    removed: Array<any> = [];
+    addTodoForm: any;
+    onLoad: any;
+    toggleErrors: boolean;
+
+    constructor(
+        private fb: FormBuilder
+    ) {
+    }
+
+    ngOnInit(): void {
+        this.init();
+
+    }
+
+    init() {
+        this.onLoad = JSON.parse(localStorage.getItem('todos'));
+        this.initForms();
+
+        if (localStorage.getItem('todos')) {
+            const retrievedData = localStorage.getItem('todos');
+            this.todos = JSON.parse(retrievedData);
+        } else {
+            this.save();
+        }
+    }
+
+    save() {
+        localStorage.setItem('todos', JSON.stringify(this.todos));
+    }
+
+    initForms() {
+        this.addTodoForm = this.fb.group({
+            todoName: ['', Validators.required],
+            todoDate: ['']
+        });
+    }
+
+    addTodo() {
+        console.log(this.addTodoForm);
+        if (this.addTodoForm.status === 'VALID') {
+            this.todos.push(
+                {
+                    name: this.addTodoForm.value.todoName,
+                    date: this.formatDate(this.addTodoForm.value.todoDate),
+                    isDone: false
+                }
+            );
+            this.toggleErrors = false;
+            this.save();
+            this.initForms();
+        } else {
+            this.toggleErrors = true;
+        }
+    }
+
+    removeTodo(index) {
+        this.removed = [];
+        this.todos[index].removeIndex = index;
+        this.removed.push(this.todos[index]);
+        this.todos.splice(index, 1);
+        this.save();
+    }
+
+    formatDate(customDate) {
+        if (customDate !== '') {
+            customDate = customDate.toString();
+            console.log(customDate);
+            const rawDate = customDate.split(' ');
+            const day = rawDate[2];
+            const month = rawDate[1];
+            const year = rawDate[3];
+
+            return `${day} ${month} ${year}`;
+        } else {
+            return '';
+        }
+    }
+
+    drop(event: CdkDragDrop<string[]>) {
+        moveItemInArray(this.todos, event.previousIndex, event.currentIndex);
+        this.save();
+    }
+
+    toggleChecked(index) {
+        const current = this.todos[index];
+
+        if (current.isDone) {
+            current.isDone = false;
+        } else {
+            current.isDone = true;
+        }
+
+        this.save();
+    }
+
+    undoItem() {
+        const index = this.removed[0].removeIndex;
+        this.todos.splice(index, 0, this.removed[0]);
+        this.removed = [];
+        this.save();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatIconModule} from '@angular/material/icon';
+import {MatNativeDateModule} from '@angular/material';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        DragDropModule,
+        MatCheckboxModule,
+        MatDatepickerModule,
+        MatIconModule,
+        MatNativeDateModule,
+        ReactiveFormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
